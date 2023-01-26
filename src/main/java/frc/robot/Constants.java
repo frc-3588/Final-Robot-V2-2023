58d284e23280adad35758b90ca6849feae530b38
lhs-robotics <lindberghrobotics@gmail.com>
2023-01-26 15:15:23 -0800

    Update auto

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -8,30 +8,42 @@
 // update. Deleting the comments indicating the section will prevent
 // it from being updated in the future.
 
-
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
+
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants.  This class should not be used for any other purpose.  All constants should be
- * declared globally (i.e. public static).  Do not put anything functional in this class.
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be
+ * declared globally (i.e. public static). Do not put anything functional in
+ * this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
  * constants are needed, to reduce verbosity.
  */
 public class Constants {
-    public static final class JoystickConstants{
+    public static final class JoystickConstants {
         public static final int kLeftStickY = 1;
         public static final int kRightStickX = 3;
     }
-   /**
-    * public static final class DriveConstants {
-    *   public static final int kLeftMotor1Port = 0;
-    *   public static final int kLeftMotor2Port = 1;
-    *   public static final int kRightMotor1Port = 2;
-    *   public static final int kRightMotor2Port = 3; 
-    * }
-    */ 
-    public final static int contant = 1;
-}
 
+    public static final class   DriveConstants {
+        public static final int kLeftMotor1Port = 0;
+        public static final int kLeftMotor2Port = 1;
+        public static final int kRightMotor1Port = 2;
+        public static final int kRightMotor2Port = 3;
+        // TODO: UPDATE THIS TO OUR TRACK WIDTH, CODE WILL NOT WORK WITHOUT
+        public static final double kTrackWidth = 0.381 * 2;
+        // TODO: SET TO WHEEL DIAMATER IN METERS
+        public static final double kWheelDiamater = 0.1016;
+    }
+
+    public final static Transform3d robotToCam = new Transform3d(new Translation3d(0.5, 0.0, 05),
+            new Rotation3d(0, 0, 0));
+}
--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -1,1 +1,52 @@
+package frc.robot.subsystems;
 
+import java.util.Optional;
+
+import org.photonvision.EstimatedRobotPose;
+import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonPoseEstimator;
+import org.photonvision.PhotonPoseEstimator.PoseStrategy;
+import org.photonvision.targeting.PhotonTrackedTarget;
+
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.geometry.Pose2d;
+import frc.robot.Constants;
+
+public class Vision {
+    private final PhotonCamera camera;
+    private AprilTagFieldLayout field;
+    private PhotonPoseEstimator poseEstimator;
+
+    public Vision() {
+        camera = new PhotonCamera("USB_Camera");
+        try {
+            field = AprilTagFieldLayout.loadFromResource(AprilTagFields.k2023ChargedUp.m_resourceFile);
+            poseEstimator = new PhotonPoseEstimator(field, PoseStrategy.CLOSEST_TO_REFERENCE_POSE, camera,
+                Constants.robotToCam);
+        } catch (Exception e) {
+            System.out.println("Field layout not found" + e);
+        }
+
+        
+    }
+
+    // A help function to log found camera targets to the console
+    public void getCameraData() {
+        var result = camera.getLatestResult();
+
+        if (result.hasTargets()) {
+            PhotonTrackedTarget target = result.getBestTarget();
+
+            int targetId = target.getFiducialId();
+            double poseAmbiguity = target.getPoseAmbiguity();
+
+            System.out.println("Target ID: " + targetId + " Pose Ambiguity: " + poseAmbiguity);
+        }
+    }
+
+    public Optional<EstimatedRobotPose> getEstimatedGlobalPose(Pose2d prevEstimatedRobotPose) {
+        poseEstimator.setReferencePose(prevEstimatedRobotPose);
+        return poseEstimator.update();
+    }
+}
